97bc11c1040b435581e7b4ddd68c5a615e09b644
zozmoh <zozk@live.com>
2022-11-11 11:39:55 +0100

    Update BlackJack.hs

--- Lab-2/BlackJack.hs
+++ Lab-2/BlackJack.hs
@@ -8,12 +8,13 @@ module BlackJack where
 import Cards
     ( Hand(..),
       Suit(Clubs, Hearts, Spades, Diamonds),
-      Rank(Ace, Jack, Numeric),
+      Rank(Ace, King, Queen, Jack, Numeric),
       Card(..),
       rank,
       size )
 import RunGame ( Player(..) )
 import Test.QuickCheck ()
+import System.Random
 
 -------------------------------------------------------------------------
 -- A0
@@ -96,15 +97,6 @@ winner guest bank  | not (gameOver guest) && gameOver bank = Guest
                    | otherwise = Bank
 
 
-
-
--------------------------------------------------------------------------
--- B0
--------------------------------------------------------------------------
-(<+) :: Hand -> Hand -> Hand
-(<+) Empty Empty = Empty
-(<+) hand Empty = hand
-
 -- We created some example hands for checking our functions
 hand2 :: Hand
 hand2 = Add (Card (Numeric 2) Hearts)
@@ -117,3 +109,87 @@ card2 = Card (Numeric 2) Hearts
 acesHand :: Hand
 acesHand = Add (Card Ace Hearts)
                 (Add (Card Ace Spades) Empty)
+
+-------------------------------------------------------------------------
+-- B1
+-------------------------------------------------------------------------
+(<+) :: Hand -> Hand -> Hand
+(<+) Empty Empty = Empty
+(<+) hand Empty = hand
+(<+) Empty hand = hand
+(<+) (Add c1 h1) h2 = Add c1 (h1 <+ h2)
+
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 =
+      p1<+(p2<+p3) == (p1<+p2)<+p3
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 =
+      size (p1<+p2) == size p1 + size p2
+
+-------------------------------------------------------------------------
+-- B2
+-------------------------------------------------------------------------
+-- It's a mess, but its works!
+fullDeck :: Hand 
+fullDeck = startFullDeck Empty
+
+startFullDeck :: Hand -> Hand 
+startFullDeck hand = deckHelper listOfHands hand
+  where listOfHands = [Add c Empty | c <- 
+                      [Card r s | r <- [Ace, King, Queen, Jack], s <- allSuits] ++ 
+                      [Card r s | r <- [Numeric n | n <- [2..10]], s <- allSuits] ]
+        allSuits = [Spades, Hearts, Diamonds, Clubs]
+
+deckHelper :: [Hand] ->  Hand -> Hand 
+deckHelper [] hand = hand
+deckHelper (x:xs) hand = x <+ (deckHelper xs hand)
+
+
+-----------------------------------------------
+-- B3 
+-----------------------------------------------
+
+draw :: Hand -> Hand -> (Hand, Hand)
+draw Empty p1 = error  "The deck is empty."
+draw (Add c deck) p1 = (deck, Add c p1) -- Drawing the first card into the p1 hand
+
+
+-----------------------------------------------
+-- B4 
+-----------------------------------------------
+second :: (a, b) -> b
+second (x,y) = y
+
+playBank :: Hand -> Hand
+playBank deck = second $ playBankHelper deck Empty
+
+playBankHelper :: Hand -> Hand -> (Hand, Hand) 
+playBankHelper deck hand | value hand < 16 = playBankHelper smallerDeck biggerHand
+                         | otherwise = (deck, hand)
+  where (smallerDeck, biggerHand) = draw deck hand
+
+-----------------------------------------------
+-- B5
+-----------------------------------------------
+-- Wants a g a deck and an empty hand
+-- and spits out a shuffled hand
+shuffleDeck :: StdGen -> Hand -> Hand -> Hand
+-- shuffleDeck g Empty sd = sd 
+shuffleDeck g h = undefined
+
+removeNthCard :: StdGen -> Hand -> (Card, Hand)
+removeNthCard g deck = undefined
+
+prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
+prop_shuffle_sameCards g h c = undefined
+  --c `belongsTo` h == c `belongsTo` shuffleDeck g h
+
+prop_size_shuffle :: StdGen -> Hand -> Bool
+prop_size_shuffle = undefined
+--prop_size_shuffle g h = size (shuffleDeck g h) == size h 
+
+-- Build a list of hands
+buildListOfHands :: Hand -> [Hand]
+buildListOfHands Empty = []
+buildListOfHands (Add card hand) = (Add card Empty):buildListOfHands hand
