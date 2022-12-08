0f94d17c6d57ad01b0d36dbd8c36c744e663562a
zozmoh <zozk@live.com>
2022-12-08 13:58:20 +0100

    Part A and B has been updated

--- Lab-4/Expr.hs
+++ Lab-4/Expr.hs
@@ -3,6 +3,7 @@
    Authors: Zozk Mohamed and Anton Sandberg
    Lab group: - 48
  -}
+{-# LANGUAGE InstanceSigs #-}
 
 import Data.Aeson.Encoding (value)
 
@@ -17,14 +18,12 @@ data Expr = Num Double
             | Mul Expr Expr 
             | Sin Expr 
             | Cos Expr 
-            | Var X
-    deriving (Show, Eq)
-
-type X = Char
+            | X
+    deriving Eq
 
 -- In order that we can run tests 
 x :: Expr
-x = Var 'x'
+x = X
 
 num :: Double -> Expr
 num = Num
@@ -40,7 +39,7 @@ cos = Cos
 -- Counts the number of functions and operators in the given expression
 size :: Expr -> Int
 size (Num _) = 1
-size (Var _) = 1
+size X = 1
 size (Add e1 e2) = 1+size e1+size e2 
 size (Mul e1 e2) = 1+size e1 +size e2
 size (Sin e) = 1+size e
@@ -53,19 +52,34 @@ size (Cos e) = 1+size e
 -- Converts any expression to a string
 showExpr :: Expr -> String
 showExpr (Num n) = show n
-showExpr (Sin e) = "sin " ++ showExpr e
-showExpr (Cos e) = "cos " ++ showExpr e
-showExpr (Var x) = show x
+showExpr (Sin e) = "sin " ++ showFactorCosSin e
+showExpr (Cos e) = "cos " ++ showFactorCosSin e
+showExpr X = "x"
 showExpr (Add e1 e2) = showExpr e1 ++ " + " ++ showExpr e2
 showExpr (Mul a b) = showFactor a ++ "*" ++ showFactor b
      where showFactor :: Expr -> String
            showFactor (Add a b) = "("++showExpr (Add a b)++")"
            showFactor e = showExpr e
 
+showFactorCosSin :: Expr -> String
+showFactorCosSin (Num n) = showExpr (Num n)
+showFactorCosSin (Sin a) = showExpr (Sin a)
+showFactorCosSin (Cos a) = showExpr (Cos a)
+showFactorCosSin X = showExpr X
+showFactorCosSin e = "(" ++ showExpr e ++ ")"
+
+instance Show Expr where
+  show :: Expr -> String
+  show = showExpr
+
+-------------------------------------------------------------
+-- *C
+-------------------------------------------------------------
+
 -- Given an expression, and the value for the variable x, calculates the value of the expression
 eval :: Expr -> Double -> Double
 eval (Num n) _ = n
-eval (Var x) v = v
+eval X v = v
 
 eval (Add e1 e2) v = eval e1 v + eval e2 v
 eval (Mul e1 e2) v = eval e1 v * eval e2 v
@@ -73,17 +87,11 @@ eval (Sin e) v = Prelude.sin $ eval e v
 eval (Cos e) v = Prelude.cos $ eval e v
 
 -------------------------------------------------------------
--- *C
--------------------------------------------------------------
-
-
-
-
-
--------------------------------------------------------------
 -- *D
 -------------------------------------------------------------
 
+readExpr :: String -> Maybe Expr
+readExpr = undefined
 
 
 
